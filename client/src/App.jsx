15d82e8cd961c64f1780239bb53880565a7ab72a
Mohammed Parves <mdperrfan@gmail.com>
2025-01-03 18:52:54 +0600

    Folder and page structure home and login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import {Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import VerifyEmail from './Pages/VerifyEmail'
+import ResetPass from './Pages/ResetPass'
+import Navbar from './Components/Navbar'
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='login' element={<Login/>}/>
+        <Route path='/verify-email' element={<VerifyEmail/>}/>
+        <Route path='/reset-pass' element={<ResetPass/>}/>
+      </Routes>
     </div>
   )
 }
