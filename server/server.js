b4c7a43a1cb13e04f7d3298f69ce819821fd5e60
Mohammed Parves <mdperrfan@gmail.com>
2025-05-13 21:42:51 +0600

    Added Frontend link for cors

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ const app = express();
 const port = process.env.PORT || 4000;
 dotenv.config();
 const allowedOrigins = [
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://mern-authentication-nu.vercel.app/'
 ]
 app.use(express.json());
 app.use(cookieParser());
